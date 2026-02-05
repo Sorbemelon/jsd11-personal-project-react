fb91dafa015f462d797bfdb8c79dd932dd85aee8
sorbemelon <67460064+Sorbemelon@users.noreply.github.com>
2026-02-05 16:21:25 +0700

    Update text color gradient of app name

--- src/components/ui/aurora-text.jsx
+++ src/components/ui/aurora-text.jsx
@@ -3,7 +3,7 @@ import React, { memo } from "react"
 export const AuroraText = memo(({
   children,
   className = "",
-  colors = ["#FF0080", "#7928CA", "#0070F3", "#38bdf8"],
+  colors = ["#51ff00", "#CCFF00", "#fff700","#ff8c00", "#ff0000"],
   speed = 1
 }) => {
   const gradientStyle = {
